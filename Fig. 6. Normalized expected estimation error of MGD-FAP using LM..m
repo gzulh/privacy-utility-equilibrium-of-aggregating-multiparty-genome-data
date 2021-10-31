90703318e8e76ba2b5bc42f133255c30faf7036a
gzulh <93442917+gzulh@users.noreply.github.com>
2021-10-31 08:08:15 +0800

    Add files via upload

---
+++ Fig. 6. Normalized expected estimation error of MGD-FAP using LM..m
@@ -0,0 +1,119 @@
+%============================MGD-FAP-LM, U=0.4=============================
+
+U=0.4;
+n=165;
+m=500;
+D1=D.';
+D2=D1(:);
+T=D2.';
+M=U*n*m;
+N=n*m-M;
+s=0;
+len=10;
+epsilon=0.1:11;
+for j=1:len
+for i=1:length(epsilon)
+    mu=0;
+    b(i)=2/epsilon(i);
+    a=rand(10*n,10*m)-0.5;
+    Y1=mu-b(i)*sign(a).*log(1-2*abs(a));
+        
+    Y2=Y1(find(mod(round(Y1),3)==0));
+    T1=Y2(1:M);
+    Y3=Y1(find(mod(round(Y1),3)~=0));
+    T2=Y3(1:N);
+    T3=[T1.' T2.'];
+       
+    C=T3(randperm(n*m));
+    P1=0.5*(1+sign(C-mu).*(1-exp(-abs(C-mu)/b(i))));
+    G=mod(T+round(C),3);
+    F=G-T;
+    S(i)=sum(P1.*abs(F))/(n*m);
+end
+    s=s+S;
+end
+plot(epsilon,s/len,'-bs','LineWidth',1.5);
+hold on;
+xlabel('Desired Privacy Budget (\epsilon)'); ylabel('Normalized Expected Estimation Error');
+
+%============================MGD-FAP-LM, U=0.6=============================
+
+U=0.6;
+n=165;
+m=500;
+D1=D.';
+D2=D1(:);
+T=D2.';
+M=U*n*m;
+N=n*m-M;
+s=0;
+len=10;
+epsilon=0.1:11;
+for j=1:len
+for i=1:length(epsilon)
+    mu=0;
+    b(i)=2/epsilon(i);
+    a=rand(10*n,10*m)-0.5;
+    Y1=mu-b(i)*sign(a).*log(1-2*abs(a));
+        
+    Y2=Y1(find(mod(round(Y1),3)==0));
+    T1=Y2(1:M);
+    Y3=Y1(find(mod(round(Y1),3)~=0));
+    T2=Y3(1:N);
+    T3=[T1.' T2.'];
+       
+    C=T3(randperm(n*m));
+    P1=0.5*(1+sign(C-mu).*(1-exp(-abs(C-mu)/b(i))));
+    G=mod(T+round(C),3);
+    F=G-T;
+    S(i)=sum(P1.*abs(F))/(n*m);
+end
+    s=s+S;
+end
+plot(epsilon,s/len,'-ro','LineWidth',1.5);
+hold on;
+xlabel('Desired Privacy Budget (\epsilon)'); ylabel('Normalized Expected Estimation Error');
+
+%============================MGD-FAP-LM, U=0.8=============================
+
+U=0.8;
+n=165;
+m=500;
+D1=D.';
+D2=D1(:);
+T=D2.';
+M=U*n*m;
+N=n*m-M;
+s=0;
+len=10;
+epsilon=0.1:11;
+for j=1:len
+for i=1:length(epsilon)
+    mu=0;
+    b(i)=2/epsilon(i);
+    a=rand(10*n,10*m)-0.5;
+    Y1=mu-b(i)*sign(a).*log(1-2*abs(a));
+        
+    Y2=Y1(find(mod(round(Y1),3)==0));
+    T1=Y2(1:M);
+    Y3=Y1(find(mod(round(Y1),3)~=0));
+    T2=Y3(1:N);
+    T3=[T1.' T2.'];
+       
+    C=T3(randperm(n*m));
+    P1=0.5*(1+sign(C-mu).*(1-exp(-abs(C-mu)/b(i))));
+    G=mod(T+round(C),3);
+    F=G-T;
+    S(i)=sum(P1.*abs(F))/(n*m);
+end
+    s=s+S;
+end
+plot(epsilon,s/len,'-gd','LineWidth',1.5);
+hold on;
+xlabel('Desired Privacy Budget (\epsilon)'); ylabel('Normalized Expected Estimation Error');
+
+
+
+xlim([0 10]);
+ylim([0 0.6]);
+legend('MGD-FAP-LM, U=0.4, n=165, m=100, 300, and 500','MGD-FAP-LM, U=0.6, n=165, m=100, 300, and 500','MGD-FAP-LM, U=0.8, n=165, m=100, 300, and 500');
